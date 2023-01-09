4fb51e7094278c1507e734ddbb4e7652f931575c
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2023-01-09 20:46:25 -0300

    header hero banner

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Banner from "./UI/Banner.svelte";
+
+</script>
+
+
+
+<Banner title = "Trabalhe no elo7"/>
+
---
+++ src/UI/Banner.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let title;
+</script>
+
+<div class="banner">
+    <div class="banner-text">
+      <h1 style="font-size:50px">{title}</h1> 
+    </div>
+  </div>
+  
+
+
+<style>
+ .banner {
+  background-image:  url("../images/foto-header.png");
+  height: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: 50%;
+ 
+}
+
+.banner-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 50%);
+  color: white;
+}
+
+
+
+</style>
