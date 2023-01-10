59d13b67be43eac12022c8de4327e4152fdbe566
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2023-01-09 22:02:58 -0300

    information component

--- src/UI/Banner.svelte
+++ src/UI/Banner.svelte
@@ -1,35 +1,31 @@
 <script>
-    export let title;
+  export let title;
 </script>
 
 <div class="banner">
-    <div class="banner-text">
-      <h1 style="font-size:50px">{title}</h1> 
-    </div>
+  <div class="banner-text">
+    <h1 style="font-size:50px">{title}</h1>
   </div>
-  
-
+</div>
 
 <style>
- .banner {
-  background-image:  url("../images/foto-header.png");
-  height: 100%;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-  background-position: 50%;
- 
-}
+   .banner {
+    background-image: url("../images/foto-header.png");
+    height: 100vh;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: 50%;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-.banner-text {
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, 50%);
-  color: white;
-}
+  }
 
-
-
+  .banner-text {
+    padding-top: 30vh;
+    text-align: center;
+    color: white;
+  }
 </style>
---
+++ src/UI/Information.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let text;
+    export let textLink;
+    export let navigate;
+
+    textLink = textLink.toUpperCase();
+
+   
+</script>
+
+
+<div class="information">
+    <p>{text}</p>
+    <hr  class="separator">
+    <a class = "link" href={navigate}>{textLink}</a>
+</div>
+
+
+<style>
+    .information{
+        padding: 20px 100px;
+        text-align: center;
+        color: rgb(100, 100, 100);
+    }
+
+    .separator{
+        width: 15vw;
+        height:2px;
+        text-align:center;
+        border:0px;
+        color:rgb(204, 203, 203); 
+        background:rgb(204, 203, 203); 
+    }
+
+    .link{
+        color: #008D8C;
+        font-weight: bold;
+    }
+
+    @media(max-width:800px){
+        .information{
+            padding: 20px 30px;
+        }
+        .separator{
+            width: 80%;
+        }
+    }
+    
+</style>
